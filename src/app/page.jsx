37c8e76caaa6f35f09ca91e60bae789f30e06d8d
ZiyadZK <kakangtea74@gmail.com>
2025-05-14 21:49:40 +0700

    update photo

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -101,7 +101,7 @@ export default function Home() {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
 
           <div className="space-y-5 border border-zinc-300/0 hover:border-zinc-300 ease-out duration-100 hover:shadow-lg rounded-xl p-5 cursor-pointer">
-            <img src={`https://assets.klikindomaret.com/products/20065778/20065778_3.webp`} className="w-full aspect-square rounded-xl object-cover object-center" alt="Foto Produk" />
+            <img src={`https://www.coca-cola.com/content/dam/onexp/id/id/brands/coca-cola/id-coca-cola.png`} className="w-full aspect-square rounded-xl object-cover object-center" alt="Foto Produk" />
             <div className="flex items-center justify-between">
               <div className="font-medium text-2xl">
                 Coca Cola
